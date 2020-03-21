8b1416544679425f57ab76cb60bcbfc7fd117b8d
raudelf <rawdell@outlook.com>
2020-03-20 21:35:36 -0500

    Completed CRUD for Projects

--- hubs/projectsHub.js
+++ hubs/projectsHub.js
@@ -4,7 +4,7 @@ const Hubs = require('../data/helpers/projectModel');
 const router = express.Router();
 
 
-// GET Requests
+// GET Request
 router.get('/:id', (req, res) => {
     const id = req.params.id;
 
@@ -24,7 +24,7 @@ router.get('/:id', (req, res) => {
 });
 
 
-// POST Requests
+// POST Request
 router.post('/', (req, res) => {
     const body = req.body;
 
@@ -40,6 +40,49 @@ router.post('/', (req, res) => {
                 console.log('There was an error while saving the post to the server.');
                 res.status(500).json({ error: 'There was an error while saving the post.'});
             };
+        });
+});
+
+
+// PUT Request
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+
+    Hubs.update(id, body)
+        .then((updated) => {
+            if (updated) {
+                if (body.name && body.description) {
+                    res.status(200).json(body);
+                } else {
+                    res.status(400).json({ errorMessage: 'Please provide a name and description for the post.'});
+                };
+            } else {
+                res.status(404).json({message: 'Project does not exist'});
+            };
+        })
+        .catch(err => {
+            console.log('There was an issue updating project to server.');
+            res.status(500).json({error: 'There was an issue updating project to server.'});
+        });
+});
+
+
+// DELETE Request
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Hubs.remove(id)
+        .then(deleted => {
+            if (deleted) {
+                res.status(200).json(deleted).end();
+            } else {
+                res.status(404).json({ errorMessage: 'The project with the specified ID does not exist.'})
+            };
+        })
+        .catch(err => {
+            console.log('The post could not be removed.');
+            res.status(500).json({ error: 'The post could not be removed.'});
         });
 });
 
